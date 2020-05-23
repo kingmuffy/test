343f7eb85bdfc48084721efdfefb8eecc0cd5e70
mofe ojei <kingmuffyatse@gmail.com>
2020-05-23 11:59:35 +0400

    add another print line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int [] myArray = array(5);
+        for (int i = 0; i<myArray.length; i++){
+            System.out.println("the array value of " +i +" is " +myArray[i]);
+        }
+
+
+
 	// write your code here
         System.out.println("hello");
+    }
+    public static int [] array(int array) {
+        int [] value = new int  [array];
+        System.out.println("enter number");
+        for (int i = 0; i<value.length; i++){
+            value[i] = scanner.nextInt();
+            System.out.println("value " +i+ " entered succesfuly");
+
+        }
+        return value;
+
     }
 }
